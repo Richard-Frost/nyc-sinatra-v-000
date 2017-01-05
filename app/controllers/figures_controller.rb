13ac543dc2a385555420fb39f9be439024fc691d
Richard-Frost <frost.rchrd@gmail.com>
2017-01-04 22:52:59 -0800

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,2 +1,47 @@
+
 class FiguresController < ApplicationController
+
+  get '/figures' do 
+    @figures = Figure.all 
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do 
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(id: params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by(id: params[:id])
+    erb :'figures/edit'
+   end
+
+  post '/figures' do
+    @figure = Figure.create(params["figure"])
+    @figure.title_ids << params[:figure][:title_ids]
+    @figure.landmark_ids << params[:figure][:landmark_ids]
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+  end
+
+  post '/figures/:id' do 
+    @figure = Figure.find_by(id: params[:id])
+    @figure.name = params[:figure][:name]
+    @figure.save
+    if !params[:landmark][:name].empty?
+    @figure.landmark.name << params["landmark"]["name"]
+  end
+    redirect to "/figures/#{@figure.id}"
+  end
 end
+
+
--- app/models/figure_title.rb
+++ app/models/figure_title.rb
@@ -3,3 +3,7 @@ class FigureTitle < ActiveRecord::Base
   belongs_to :title
 end
 
+#george_w = Figure.create(name: "George Washington")
+#cherry_tree = Landmark.create(name: "Cherry Tree", figure_id: george_w.id, year_completed: 1738)
+#president = Title.create(name: "president")
+#FigureTitle.create(title_id: president.id, figure_id: george_w.id)
